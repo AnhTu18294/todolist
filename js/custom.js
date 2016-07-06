982e633dd1540a2e2e294536704d69cdffd51e89
AnhTu18294 <tu.na.1821994@gmail.com>
2016-07-06 16:20:52 +0700

    full function todolist

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,50 @@
 $(document).ready(function(){
   
   var $btnAdd = $("#btnAdd");
-  var $btnDel = $("#del");
+  var $btnDel = $("#btnDel");
   var $tbJob  = $("#tbJob");
   var $listJobs = $("#listJobs")
   
+  var index = 0;
   $btnAdd.click(addJobToList);
+  $btnDel.click(delListJobsChecked);
+
+  $("#cbTest").change(function(){
+    if($(this).is(":checked")) {
+        $("#tbTest").css('text-decoration','line-through');
+    } 
+  });
+
   function addJobToList(){
     if($tbJob.val() != ""){
-        $listJobs.append("<li>"+$tbJob.val()+"<input type = 'checkbox' /><a href = ''>Del</a></li>");
-        
+        index += 1;
+        $listJobs.append("<li>"+$tbJob.val()+"<input type = 'checkbox' class = 'delete' id = 'check_"+index+"'' /><button id = 'btnDel_"+index+"' >Del</button></li>");
+        $("#btnDel_"+index).click(delJob);
+        $("#check_"+index).change(function(){
+            if($(this).is(":checked")) {
+                $(this).parent().css('text-decoration','line-through');
+            } 
+        });
     }
   };
+
+  function delJob(){
+    console.log($(this).parent());
+    $(this).parent().remove();
+  };
+
+  function doneJob(){
+    
+  };
+
+  function delListJobsChecked(){
+    $(".delete").each(function(){
+        console.log($(this));
+        if($(this).is(":checked")){
+        $(this).parent().remove();
+        }
+    });    
+  };
+
+
 });
