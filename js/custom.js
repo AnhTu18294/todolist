e9710460cc9cd661b7f155687b4e8948c97c6a61
AnhTu18294 <tu.na.1821994@gmail.com>
2016-07-06 14:36:22 +0700

    up code

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,15 @@
 $(document).ready(function(){
-    var i = 0;
-    $("#btnAdd").click(function(){
-    	var job = $("#job").val();    	
-    	if(job != ""){
-            i = i+1;    		
-    		$("#listJobs").append("<li>"+job+"<input type = 'checkbox' class = 'delete'></li>"); 
-        }       
-    });
-    $("#listJobs").find("li").click(function(){        
-        $(this).remove();
-        console.log("removed")
-    });  
-
-    $("#btnDel").click(function(){
-        $(".delete").each(function(){
-            console.log($(this));
-            if($(this).is(":checked")){
-                $(this).parent().remove();
-            }
-        });
-    });
-    $("#job").keypress(function(event){
-        if(event.which == 13){
-            var job = $("#job").val();      
-            if(job != ""){
-                i = i+1;            
-                $("#listJobs").append("<li id = '"+i+"'>"+job+"<input type = 'checkbox' class = 'delete'></li>"); 
-            }     
-        }
-    });
-       
+  
+  var $btnAdd = $("#btnAdd");
+  var $btnDel = $("#del");
+  var $tbJob  = $("#tbJob");
+  var $listJobs = $("#listJobs")
+  
+  $btnAdd.click(addJobToList);
+  function addJobToList(){
+    if($tbJob.val() != ""){
+        $listJobs.append("<li>"+$tbJob.val()+"<input type = 'checkbox' /><a href = ''>Del</a></li>");
+        
+    }
+  };
 });
